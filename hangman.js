cf4decd873542078bd06b2cbf5b5ee4865cdb48e
Veronica Hernandez <vlhernandez@gmail.com>
2016-05-09 10:22:16 -0500

    capturing and determining if guessed letters are correct

--- hangman.js
+++ hangman.js
@@ -1,13 +1,15 @@
 $( document ).ready(function() {
+  var word = "";
+  var wordObj = {};
+
   function getWord() {
-    var word = $.ajax({
+    return $.ajax({
       url: "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=5&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=8&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
       type: "GET",
       data: {},
       dataType: "json",
 
      success: function(data) {
-        var wordObj = {};
 
         data = data.word.split("");
 
@@ -23,13 +25,40 @@ $( document ).ready(function() {
 
     error: function(err) {alert(err);}
     })
-
-     return word;
   }
 
 
   $("button").click(function( event ) {
-     alert("Yay! You clicked me! Getting your word");
-     getWord();
+     console.log("Getting your word");
+     word = getWord();
+     console.log("word is...", word);
   });
+
+  $(".key").on("click", function( event ) {
+    var theLetterIs= $(this).html();
+    console.log("You clicked on: ",theLetterIs);
+    $(this).off(); //don't allow anymore clicks
+    $(this).fadeTo("fast", 0.5, function() {
+      console.log("complete keyboard key fadeout");
+    });
+    checkWord(theLetterIs);
+  });
+
+
+  function checkWord(letter) {
+    console.log("in check word");
+    console.log("word object is...", wordObj);
+    for (var key in wordObj) {
+      if (letter === wordObj[key].val) {
+        console.log("found that letter...", letter)
+        wordObj[key].guessed = true;
+        findTheLetter(key);
+      }
+    }
+    console.log("word object is...", wordObj);
+  }
+
+  function findTheLetter(indx) {
+    ($("[idx='"+ indx +"']")).css("background-color", "lightgreen");
+  };
 });

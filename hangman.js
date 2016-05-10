3c8782add85423aa8714a541e4e7f19ebc62a753
Veronica Hernandez <vlhernandez@gmail.com>
2016-05-10 10:09:57 -0500

    disable keys after a guess

--- hangman.js
+++ hangman.js
@@ -3,13 +3,15 @@ $( document ).ready(function() {
   var wordObj = {};
 
   function getWord() {
-    return $.ajax({
+    $.ajax({
       url: "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=5&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=8&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
       type: "GET",
       data: {},
       dataType: "json",
 
      success: function(data) {
+        word = data.word;
+        console.log('word is...', word);
 
         data = data.word.split("");
 
@@ -18,7 +20,7 @@ $( document ).ready(function() {
         })
         console.log("wordobj...", wordObj)
         $.each(data, function(idx, letter) {
-          $("#word").append("<div idx="+idx+" class=letter>"+letter+"</div>");
+          $("#word").append("<div idx="+idx+" class=letter></div>");
         })
 
       },
@@ -30,17 +32,15 @@ $( document ).ready(function() {
 
   $("button").click(function( event ) {
      console.log("Getting your word");
-     word = getWord();
-     console.log("word is...", word);
+     getWord();
   });
 
   $(".key").on("click", function( event ) {
     var theLetterIs= $(this).html();
     console.log("You clicked on: ",theLetterIs);
     $(this).off(); //don't allow anymore clicks
-    $(this).fadeTo("fast", 0.5, function() {
-      console.log("complete keyboard key fadeout");
-    });
+    $(this).fadeTo("fast", 0.25);
+    $(this).removeClass("enabled");
     checkWord(theLetterIs);
   });
 
@@ -52,13 +52,17 @@ $( document ).ready(function() {
       if (letter === wordObj[key].val) {
         console.log("found that letter...", letter)
         wordObj[key].guessed = true;
-        findTheLetter(key);
+        findTheLetter(key, letter);
       }
     }
     console.log("word object is...", wordObj);
   }
 
-  function findTheLetter(indx) {
-    ($("[idx='"+ indx +"']")).css("background-color", "lightgreen");
+  function findTheLetter(indx, letter) {
+    $("[idx='"+ indx +"']").html(letter);
   };
+
+  $("body").keypress(function(event) {
+    console.log("a key was pressed", event);
+  })
 });
